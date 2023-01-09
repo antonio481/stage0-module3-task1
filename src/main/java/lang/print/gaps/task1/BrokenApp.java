6e2a3b2b012e822ceabcb460d381f3492046656f
Anton Danilov <adanilovx@gmail.com>
2023-01-09 21:28:39 +0400

    Decision commit

--- src/main/java/lang/print/gaps/task1/BrokenApp.java
+++ src/main/java/lang/print/gaps/task1/BrokenApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class BrokenApp {
     public static void main(String[] args){
-        System.out.println("fix");
-        System.out.println("Me");
+        System.out.println("fixMe\n");
     }
 }
